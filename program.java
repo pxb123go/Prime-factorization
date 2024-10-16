055f112c31447a4670d9b3e97de02ccdcaf6868a
pxb123go <sahoopradyun@gmail.com>
2024-10-16 20:11:01 +0300

    initial commit

---
+++ program.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class program{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=2; i*i<=n; i++){
+while (n%i==0) {
+    n=n/i;
+    System.out.println(i+" ");
+}
+        }
+        if (n!=1) {
+            System.out.println(n);
+}
+        }
+    }
